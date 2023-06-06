071d0627460cdea809b763e89e2a0f2df3c3e72a
txno376 <txtxeon3376@gmail.com>
2023-06-06 21:11:19 +0800

    README

--- sxt.py
+++ sxt.py
@@ -5,7 +5,7 @@ from handright import Template,handwrite
 text =open("date.txt",'r',encoding='utf-8').read()
 template = Template(
     background=Image.new(mode="1", size=(1080, 2340), color=1),
-    font=ImageFont.truetype("font.ttf", size=100),
+    font=ImageFont.truetype("ziti/font.ttf", size=100),
 )
 images = handwrite(text, template)
 for im in images:
